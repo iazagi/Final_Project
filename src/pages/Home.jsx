64f8d83d1cac46325df84b3ea69190bc058b4d00
iazagi <101048623+iazagi@users.noreply.github.com>
2022-06-07 12:18:34 +0300

    end trnding

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,18 @@
-import React from "react";
-import LiveAuction from "../components/ui/Live-auction/LiveAuction";
+import React from 'react';
+import LiveAuction from '../components/ui/Live-auction/LiveAuction';
 import HeroSection from '../components/ui/HeroSection';
-import SellerSection from "../components/Header/Seller-section/SellerSection";
+import SellerSection from '../components/Header/Seller-section/SellerSection';
+import TrendingSection from '../components/ui/Trending-Section/TrendingSection';
 
-const Home = () =>{
-    return(
-        <>
-            <HeroSection />
-            <LiveAuction />
-            <SellerSection />
-        </>
-    )  
+const Home = () => {
+  return (
+    <>
+      <HeroSection />
+      <LiveAuction />
+      <SellerSection />
+      <TrendingSection />
+    </>
+  );
 };
 
 export default Home;
